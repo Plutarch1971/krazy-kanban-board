06398fe67360c4df795028d509f98bd318deee9c
Matt-Verni <matthewpmendez@gmail.com>
2024-11-09 20:33:12 -0500

    Application Running Successfully

--- server/dist/routes/auth-routes.js
+++ server/dist/routes/auth-routes.js
@@ -24,4 +24,5 @@ export const login = async (req, res) => {
 const router = Router();
 // POST /login - Login a user
 router.post('/login', login);
+//router.post('/signup', signup);
 export default router;
--- server/src/routes/auth-routes.ts
+++ server/src/routes/auth-routes.ts
@@ -30,5 +30,6 @@ const router = Router();
 
 // POST /login - Login a user
 router.post('/login', login);
+//router.post('/signup', signup);
 
 export default router;
